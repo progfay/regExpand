d6aeae0de0595b081240f820ef18043882388eb8
progfay <shun1856shun@gmail.com>
2018-11-05 19:58:41 +0900

    feat: remove duplicate strings by filter

--- src/recursively.js
+++ src/recursively.js
@@ -3,9 +3,10 @@ const expand = (pattern, dict = null) => {
   const match = /^(?<prefix>.*?)\((?<expression>[^\(\)]*)\)(?<option>\??)(?<suffix>.*)$/.exec(pattern)
   if (!match) return pattern.split(`|`)
   const { prefix, expression, option, suffix } = match.groups
-  return Array.from(new Set(expand(expression + (option ? '|' : ''))
+  return expand(expression + (option ? '|' : ''))
     .map(e => expand(prefix + e + suffix))
-    .reduce((a, b) => [...a, ...b])))
+    .reduce((a, b) => [...a, ...b])
+    .filter((x, i, self) => self.indexOf(x) === i)
 }
 
 console.log(expand('{num}(a|b|(c|d)|e|f(g|h)?){num}', { num: '(0|1|2|3|4|5|6|7|8|9)' }))
