f9c26178e90e22bf0cd26f79cbf6c0b81ff9d242
progfay <shun1856shun@gmail.com>
2018-11-05 17:01:05 +0900

    feat: parse expression recursively

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ const getPairClosingBracketIndex = (pattern) => {
       if (count === 0) return closing
       closing = pattern.indexOf(')', closing + 1)
     }
-    console.log({opening, closing, count})
     if (closing === -1) return -1
   }
 }
@@ -22,17 +21,31 @@ const scan = (pattern) => {
   const openingBracketIndex = pattern.indexOf('(')
   switch (openingBracketIndex) {
     case -1:
-      return [pattern]
+      return pattern
 
     case 0:
       const pairClosingBracketIndex = getPairClosingBracketIndex(pattern)
-      return [pattern.substring(0, pairClosingBracketIndex + 1)]
-        .concat(scan(pattern.substring(pairClosingBracketIndex + 1)))
+      return [scan(pattern.substring(0, pairClosingBracketIndex + 1).slice(1, -1))]
+        .concat(scan(pattern.substring(pairClosingBracketIndex + 1)) || [])
 
     default:
-      console.log(pattern.substring(openingBracketIndex))
-      return [pattern.substring(0, openingBracketIndex)].concat(scan(pattern.substring(openingBracketIndex)))
+      return [pattern.substring(0, openingBracketIndex)].concat(scan(pattern.substring(openingBracketIndex)) || [])
   }
 }
 
-console.log(scan('((())((()))((())()))adfadsfadsf((()())())()'))
+const parse = (element) => {
+  if (typeof element === 'string') {
+    return element
+      .split('|')
+      .filter(s => s)
+  } else {
+    return element.map(parse)
+  }
+}
+
+const expand = (pattern) => {
+  return scan(pattern)
+    .map(parse)
+}
+
+console.log(expand('(a|b|c|d|e|(f|g))-(1|2|3|4|5)'))
