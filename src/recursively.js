528b1eae92243eeed6851f227dcccc56e94514f4
progfay <shun1856shun@gmail.com>
2018-11-10 12:44:39 +0900

    feat: split the recursive func

--- src/recursively.js
+++ src/recursively.js
@@ -1,5 +1,15 @@
 const regExp = /^(.*?)\(([^\(\)]*)\)(\??)(.*)$/
 
+const _expand = (pattern) => {
+  const match = regExp.exec(pattern)
+  if (!match) return pattern.split(`|`)
+  const [, prefix, expression, option, suffix] = match
+  return _expand(expression + (option ? '|' : ''))
+    .map(exp => _expand(prefix + exp + suffix))
+    .reduce((a, b) => [...a, ...b])
+    .filter((x, i, self) => self.indexOf(x) === i)
+}
+
 const expand = (pattern, dict = null) => {
   if (dict) {
     pattern = pattern.replace(/\{.*?\}/g, key => {
@@ -8,13 +18,7 @@ const expand = (pattern, dict = null) => {
     })
   }
 
-  const match = regExp.exec(pattern)
-  if (!match) return pattern.split(`|`)
-  const [, prefix, expression, option, suffix] = match
-  return expand(expression + (option ? '|' : ''))
-    .map(exp => expand(prefix + exp + suffix))
-    .reduce((a, b) => [...a, ...b])
-    .filter((x, i, self) => self.indexOf(x) === i)
+  return _expand(pattern)
 }
 
 console.log(expand('{num}(a|b|(c|d)|e|f(g|h)?)', { num: '(0|1|2|3|4|5|6|7|8|9)' }))
